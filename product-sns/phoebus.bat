caf1f8cfc30a18b0f4f121e7e70f1adc8a0ef32c
Kay Kasemir <kay.kasemir@gmail.com>
2019-05-08 09:45:37 -0400

    Windows bat

--- product-sns/phoebus.bat
+++ product-sns/phoebus.bat
@@ -6,22 +6,21 @@
 @REM replace the following with
 @REM    set JAVA_HOME=c:\path\to\that\runtime
 
-IF EXIST "..\jdk" (
+@IF EXIST "..\jdk" (
     set JAVA_HOME=..\jdk
-    @path %JAVA_HOME%\bin;%PATH%
+    @path %JAVA_HOME%\bin
 ) ELSE (
-    ECHO Cannot locate JDK
+    @ECHO Cannot locate JDK
 )
 
 @java -version
 
 @set V=0.0.1
 
-@IF EXIST product-%V%.jar (
-  SET JAR=product-%V%.jar
+@IF EXIST product-sns-%V%.jar (
+  SET JAR=product-sns-%V%.jar
 ) ELSE (
-  SET JAR=product-%V%-SNAPSHOT.jar
-
+  SET JAR=product-sns-%V%-SNAPSHOT.jar
 )
 
-@REM java -jar %JAR% %*
+@java -jar %JAR% %*
