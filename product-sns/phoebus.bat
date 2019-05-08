d4089db8cc83013efdfc956b119bc38f42a2bce1
Kay Kasemir <kay.kasemir@gmail.com>
2019-05-08 09:04:11 -0400

    Windows: Check for bundled JDK

--- product-sns/phoebus.bat
+++ product-sns/phoebus.bat
@@ -1,15 +1,27 @@
-REM Phoebus launcher for Windows
-REM When deploying, you might want to change "TOP"
-REM to the absolute installation path
+@REM Phoebus launcher for Windows
+@REM
+@REM Uses a JDK in ..\jdk
+@REM
+@REM If you want to use a specific Java Runtime,
+@REM replace the following with
+@REM    set JAVA_HOME=c:\path\to\that\runtime
 
-set V=0.0.1
-
-IF EXIST product-sns-%V%.jar (
-  SET JAR=product-sns-%V%.jar
+IF EXIST "..\jdk" (
+    set JAVA_HOME=..\jdk
+    @path %JAVA_HOME%\bin;%PATH%
 ) ELSE (
-  SET JAR=product-sns-%V%-SNAPSHOT.jar
+    ECHO Cannot locate JDK
+)
+
+@java -version
+
+@set V=0.0.1
+
+@IF EXIST product-%V%.jar (
+  SET JAR=product-%V%.jar
+) ELSE (
+  SET JAR=product-%V%-SNAPSHOT.jar
 
 )
 
-java -jar %JAR% %*
-
+@REM java -jar %JAR% %*
