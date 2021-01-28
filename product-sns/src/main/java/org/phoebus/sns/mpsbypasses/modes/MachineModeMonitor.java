ec82575f4aaa4747451868d464d1b064465884d2
Kay <ky9@ornl.gov>
2021-01-28 14:11:25 -0500

    Rxjava update

--- product-sns/src/main/java/org/phoebus/sns/mpsbypasses/modes/MachineModeMonitor.java
+++ product-sns/src/main/java/org/phoebus/sns/mpsbypasses/modes/MachineModeMonitor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 Oak Ridge National Laboratory.
+ * Copyright (c) 2019-2021 Oak Ridge National Laboratory.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -16,7 +16,7 @@ import org.phoebus.pv.PV;
 import org.phoebus.pv.PVPool;
 import org.phoebus.sns.mpsbypasses.MPSBypasses;
 
-import io.reactivex.disposables.Disposable;
+import io.reactivex.rxjava3.disposables.Disposable;
 
 /** Read machine mode from MPS PVs
  *
